2697c18c3bbd84821c1d77e750d4a6a8aec412fc
Bhagwan_Gire <bhagwangire05@gmail.com>
2024-06-28 13:14:12 +0530

    order update

--- model/Order.js
+++ model/Order.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose")
 
 const orderSchema = new mongoose.Schema({
     user: { type: mongoose.Types.ObjectId, ref: "user", required: true },
-    products: {
-        product: {
-            type: mongoose.Types.ObjectId,
-            ref: "product",
-            required: true
-        },
-        qty: {
-            type: Number,
-            required: true
+    products: [
+        {
+            product: {
+                type: mongoose.Types.ObjectId,
+                ref: "product",
+                required: true
+            },
+            qty: {
+                type: Number,
+                required: true
+            }
         }
-    },
+    ],
 
     status: {
         type: String,
