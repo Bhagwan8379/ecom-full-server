c81f15bb7c5754bd4819e1833625c5697f87f656
Bhagwan_Gire <bhagwangire05@gmail.com>
2024-06-28 12:52:09 +0530

    route Update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ const userController = require("./../controllers/user.controller")
 router
     .get("/order/:id", userController.userGetAllOrders)
     .get("/order-details/:id", userController.userGetOrderDetails)
-    .post("/placed-order", userController.userPlacedOrder)
+    .post("/order-placed", userController.userPlacedOrder)
     .put("/order-cancel/:id", userController.userCancelOrder)
     .post("/update-password/:id", userController.userUpdatePassword)
 
